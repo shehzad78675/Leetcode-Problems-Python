1d85e030d48165174f39e18601bb87688c48478d
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-09-10 16:36:46 +0500

    Time: 39 ms (77.28%) | Memory: 17.5 MB (40.70%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -7,21 +7,21 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         paths = []
-        def hasPathSum(root, target, path):
+        def treePaths(root, target, path):
             if not root:
                 return
             
             path.append(root.val)
-            if root.val - target == 0 and root.left is None and root.right is None:
+            if not root.left and not root.right and root.val == target:
                 paths.append(list(path))
                 path.pop()
                 return
 
-            left = hasPathSum(root.left, target - root.val, path)
-            right = hasPathSum(root.right, target - root.val, path)
+            treePaths(root.left, target - root.val, path)
+            treePaths(root.right, target - root.val, path)
 
-            path.pop()        
+            path.pop()
 
         path = []
-        hasPathSum(root, targetSum, path)
+        treePaths(root, targetSum, path)
         return paths
