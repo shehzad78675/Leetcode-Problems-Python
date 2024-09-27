674732e769229df47e7b524a647dd4911a8cfd0c
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-09-27 16:55:46 +0500

    Time: 42 ms (93.37%) | Memory: 16.7 MB (19.63%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,37 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
 
         q = deque()
-        vis = []
-
-        not_rotten = 0
-        for i in range(n):
-            for j in range(m):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 2:
                     q.append((i, j, 0))
-                elif grid[i][j] == 1:
-                    not_rotten += 1
-        
-        drow = [1,0,-1,0]
-        dcol = [0,-1,0,1]
-
-        cnt = 0
-        tm = 0
+        time = 0
+        dric = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         while q:
-            row, col, time = q.popleft()
-            tm = max(tm, time)
+            row, col, t = q.popleft()
+            time = max(time, t)
             for i in range(4):
-                nrow = row + drow[i]
-                ncol = col + dcol[i]
+                nrow = row + dric[i][0]
+                ncol = col + dric[i][1]
 
-                if 0 <= nrow < n and 0 <= ncol < m and grid[nrow][ncol] == 1:
+                if nrow >= 0 and nrow < m and  ncol >= 0 and ncol < n and grid[nrow][ncol] == 1:
                     grid[nrow][ncol] = 2
-                    cnt += 1
-                    q.append((nrow, ncol, time+1))
+                    q.append((nrow, ncol, t + 1))
 
-        if cnt != not_rotten:
-            return -1
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    return -1
+        
+        return time
 
-        return tm
+        
