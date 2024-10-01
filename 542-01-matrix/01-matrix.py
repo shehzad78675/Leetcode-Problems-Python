3d9e57f68bc562c86f6a7412d61e33126c0ef86b
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-01 17:01:00 +0500

    Time: 508 ms (30.98%) | Memory: 20.7 MB (14.09%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,31 +1,30 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        n = len(mat)
-        m = len(mat[0])
-
-        dist = [[0] * m for i in range(n)]
-        vis = [[0] * m for i in range(n)]
+        m = len(mat)
+        n = len(mat[0])
 
         q = deque()
-        for i in range(n):
-            for j in range(m):
+        dist = [[0] * n for i in range(m)]
+        vis = [[0] * n for i in range(m)]
+        for i in range(m):
+            for j in range(n):
                 if mat[i][j] == 0:
-                    vis[i][j] = 1
                     q.append((i, j, 0))
+                    vis[i][j] = 1
 
-        drow = [1,0,-1,0]
-        dcol = [0,-1,0,1]
-
+       
+        dric = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         while q:
             row, col, dis = q.popleft()
-
             dist[row][col] = dis
-            for i in range(4):
-                nrow = row + drow[i]
-                ncol = col + dcol[i]
 
-                if nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and vis[nrow][ncol] == 0:
-                        q.append((nrow, ncol, dis + 1))
-                        vis[nrow][ncol] = 1
+            for i in range(4):
+                nrow = row + dric[i][0]
+                ncol = col + dric[i][1]
+
+                if nrow >= 0 and nrow < m and  ncol >= 0 and ncol < n and vis[nrow][ncol] == 0:
+                
+                    vis[nrow][ncol] = 1
+                    q.append((nrow, ncol, dis + 1))
         
         return dist
