aabda64397281c4dd0e25fa0f19d1c7fa8a62186
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2024-10-03 16:55:59 +0500

    Time: 93 ms (76.86%) | Memory: 17.8 MB (61.68%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,41 +1,43 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-
-        m = len(flights)
         adj_list = {}
 
         for i in range(n):
             adj_list[i] = []
 
+        m = len(flights)
         for i in range(m):
             u = flights[i][0]
             v = flights[i][1]
             wt = flights[i][2]
 
-            adj_list[u].append([v, wt])
-
-
-        dist = [float("inf")] * n
+            adj_list[u].append((v, wt))
+        
+        
+        cost = [float("inf")] * n
         q = deque()
 
-        q.append((0, src, 0))
-        dist[src] = 0
+        cost[src] = 0
+        q.append((0, 0, src))
 
         while q:
-            stop, node, cost = q.popleft()
+            stop, cos, node = q.popleft()
 
             if stop > k:
                 continue
+            
+            for i in range(len(adj_list[node])):
+                adj_node = adj_list[node][i][0]
+                adj_wt = adj_list[node][i][1]
 
-            for flight in adj_list[node]:
-                cos = cost + flight[1]
-                if cos < dist[flight[0]] and stop <= k:
-                    dist[flight[0]] = cos
-                    q.append((stop+1, flight[0], cos))
-        
-        if dist[dst] == float("inf"):
+                if cos + adj_wt < cost[adj_node]:
+                    cost[adj_node] =  cos + adj_wt
+                    q.append((stop+1,  cos + adj_wt, adj_node))
+                
+        if cost[dst] == float("inf"):
             return -1
         
-        return dist[dst]
+        return cost[dst]
 
-        
+
+            
